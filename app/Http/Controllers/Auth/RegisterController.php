da285c891dd97e130fd6d729108b7db31a1945f8
Gustavo Camargo <camargo25.gustavo@gmail.com>
2020-09-01 12:18:03 -0300

    Remove campo level

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,6 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'level' => $data['level'],
             'password' => Hash::make($data['password']),
         ]);
     }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('level');
             $table->rememberToken();
             $table->timestamps();
         });
